b646d3e0f791d2fcb14b86bafda5ee3464f6acdd
irfan2627 <irfanmuhammed855@gmail.com>
2024-11-29 18:55:49 +0530

    c16

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -37,6 +37,7 @@ const loadLogin = async (req, res) => {
         } else {
             console.log('Error in getting USER DB');
         }
+        
         res.render('admin_login')
 
     } catch (error) {
